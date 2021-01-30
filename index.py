b1e71e097a8f38cc4c9a46cc4a5e290bf574d47f
ran <rheyannmagcalas@gmail.com>
2021-01-30 19:33:12 +0800

    Added Virtualenv Commands

--- index.py
+++ index.py
@@ -10,6 +10,24 @@ if topic == 'Data Visualization':
     st.markdown('<hr>', unsafe_allow_html=True)
 elif topic == 'Git':
     st.markdown('<hr>', unsafe_allow_html=True)
+    st.subheader('a. Cloning a repository:')
+    st.write('1. Create new repository: https://github.com/new')
+    st.write('2. Copy new repo URL')
+    st.write('3. Download: https://desktop.github.com/')
+    st.write('4. Open Github Desktop > Select Clone Repository from Internet')
+    st.write('5. Go to URL Tab > Paste new repo URL > Specify local path > Click Clone')
+
+    st.subheader('b. Push changes to github:')
+    st.write('1. Select the files to be committed')
+    st.write('2. Input your commit message and click commit')
+    st.write('3. To update github repo make sure to click publish to branch ')
+
+    st.subheader('c. Pull changes from github:')
+    st.write('1. Click fetch origin')
+    st.write('2. Click pull origin')
+
+    st.markdown('<hr>', unsafe_allow_html=True)
+    st.subheader('Git Commands')
     st.markdown('<table style="font-size:11px">'\
                 '<thead><tr><th>Command</th><th>Command</th></tr></thead>'\
                 '<tbody><td>git status</td><td>Displays the state of the working directory</td></tbody>'\
@@ -45,41 +63,18 @@ elif topic == 'Conda Virtualenv':
     st.write('Virtualenv is a tool to create isolated Python environments. ')
     
     st.subheader('Steps to Create and Use Virtualenv:')
-    st.markdown('<b>1. Create Environment:</b> <br> conda create -n <env_name> python=<python_version>', unsafe_allow_html=True)
-    st.markdown('<b>2. Check if Environment is Created:</b> <br> conda info --envs', unsafe_allow_html=True)
-    st.markdown('<b>3. Activate Environment:</b> <br>  conda activate <env_name>', unsafe_allow_html=True)
-    st.markdown('<b>4. Open Jupyter Notebook</b> <br>', unsafe_allow_html=True)
+    st.markdown('<b>1. pip install virtualenv', unsafe_allow_html=True)
+    st.markdown('<b>2. virtualenv <env_name>', unsafe_allow_html=True)
+    st.markdown('<b>3. source <env_name>/bin/activate', unsafe_allow_html=True)
+    st.markdown('<b>4. pip install ipykernel', unsafe_allow_html=True)
+    st.markdown('<b>4. sudo python -m ipykernel install --user --name <env_name> --display-name "Python (myenv)"', unsafe_allow_html=True)
+    st.markdown('<b>5. open jupyter notebook >> select the created virtualenv', unsafe_allow_html=True)
     
     st.markdown('<hr>', unsafe_allow_html=True)
-    st.markdown('<b>Save Installed Libraries:</b> pip freeze >> requirements.txt', unsafe_allow_html=True)
+    st.markdown('<b>Save Installed Libraries:</b> pip freeze > requirements.txt', unsafe_allow_html=True)
     st.markdown('<b>Install Multiple Libraries:</b> pip install -r requirements.txt', unsafe_allow_html=True)
     
     st.markdown('<hr>', unsafe_allow_html=True)
-    st.markdown('<b>Stop Enviroment:</b>&nbsp;&nbsp;&nbsp;&nbsp;conda deactivate', unsafe_allow_html=True)
-    st.markdown('<b>Delete Enviroment:</b>&nbsp;conda remove -n <env_name> --all', unsafe_allow_html=True)
+    st.markdown('<b>Delete Kernel Enviroment:</b>&nbsp;&nbsp;&nbsp;&nbsp;sudo jupyter kernelspec uninstall <env_name>', unsafe_allow_html=True)
     
-        
-        
-
-# import streamlit as st
-
-# def get_user_name():
-#     return 'John'
-
-# with st.echo():
-#     # Everything inside this block will be both printed to the screen
-#     # and executed.
-
-#     def get_punctuation():
-#         return '!!!'
-
-#     greeting = "Hi there, "
-#     value = get_user_name()
-#     punctuation = get_punctuation()
-
-    #st.write(greeting, value, punctuation)
-
-# And now we're back to _not_ printing to the screen
-# foo = 'bar'
-# st.write('Done!')
 
